b52a92724c0d2bd40c29cac1c62e2ebd71c9a4db
RealAlphaUA <realalpha.ua@gmail.com>
2021-05-26 15:26:17 +0200

    add DeducatedEnderChest 🎠²

--- src/main/java/eu/mshade/enderchest/DedicatedEnderChest.java
+++ src/main/java/eu/mshade/enderchest/DedicatedEnderChest.java
@@ -1,7 +1,64 @@
-package eu.mshadeproduction.enderchest;
+package eu.mshade.enderchest;
+
+import eu.mshade.enderchest.protocol.ProtocolRepository;
+import eu.mshade.enderchest.world.DefaultChunkGenerator;
+import eu.mshade.enderchest.world.WorldBufferIO;
+import eu.mshade.enderchest.world.WorldManager;
+import eu.mshade.enderframe.EnderFrameSession;
+import eu.mshade.enderframe.protocol.MinecraftEncryption;
+import eu.mshade.enderframe.world.*;
+import eu.mshade.enderman.EndermanProtocol;
+import io.netty.channel.EventLoopGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DedicatedEnderChest {
 
+    private final List<EnderFrameSession> enderFrameSessions = new ArrayList<>();
+    private final MinecraftEncryption minecraftEncryption = new MinecraftEncryption();
+    private final ProtocolRepository protocolRepository = new ProtocolRepository();
+    private final WorldManager worldManager;
+    private final EventLoopGroup eventLoopGroup;
 
+    public DedicatedEnderChest(EventLoopGroup eventLoopGroup) {
+        this.eventLoopGroup = eventLoopGroup;
+        this.worldManager = new WorldManager(this);
+        this.protocolRepository.register(new EndermanProtocol());
+
+        WorldLevel worldLevel = new WorldLevel("world", -4975988339999789512L, LevelType.DEFAULT, Dimension.OVERWORLD, Difficulty.NORMAL);
+
+        WorldBuffer world = this.worldManager.createWorld(worldLevel);
+        world.setChunkGenerator(new DefaultChunkGenerator(world));
+
+    }
+
+    public void addPlayer(EnderFrameSession enderFrameSession){
+        this.enderFrameSessions.add(enderFrameSession);
+    }
+
+    public void removePlayer(EnderFrameSession enderFrameSession){
+        this.enderFrameSessions.remove(enderFrameSession);
+    }
+
+    public List<EnderFrameSession> getEnderFrameSessions() {
+        return enderFrameSessions;
+    }
+
+    public ProtocolRepository getProtocolRepository() {
+        return protocolRepository;
+    }
+
+    public MinecraftEncryption getMinecraftEncryption() {
+        return minecraftEncryption;
+    }
+
+    public EventLoopGroup getEventLoopGroup() {
+        return eventLoopGroup;
+    }
+
+    public WorldManager getWorldManager() {
+        return worldManager;
+    }
 
 }
