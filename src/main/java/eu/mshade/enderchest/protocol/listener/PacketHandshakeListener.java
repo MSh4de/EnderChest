6fa30bbba4b8b288af19f0ab2f779dc7852357e9
RealAlphaUA <realalpha.ua@gmail.com>
2021-07-19 21:47:47 +0200

    update PacketHanshakeListener 🎭

--- src/main/java/eu/mshade/enderchest/protocol/listener/PacketHandshakeListener.java
+++ src/main/java/eu/mshade/enderchest/protocol/listener/PacketHandshakeListener.java
@@ -1,21 +1,24 @@
 package eu.mshade.enderchest.protocol.listener;
 
-import eu.mshade.enderchest.protocol.ProtocolRepository;
+import eu.mshade.enderchest.DedicatedEnderChest;
 import eu.mshade.enderframe.EnderFrameSessionHandler;
 import eu.mshade.enderframe.event.entity.PacketHandshakeEvent;
+import eu.mshade.enderframe.mojang.chat.TextComponent;
 import eu.mshade.enderframe.protocol.HandshakeStatus;
 import eu.mshade.enderframe.protocol.ProtocolStatus;
 import eu.mshade.enderframe.protocol.ProtocolVersion;
-import eu.mshade.mwork.dispatcher.DispatcherContainer;
-import eu.mshade.mwork.dispatcher.DispatcherListener;
+import eu.mshade.enderframe.protocol.packet.PacketOutDisconnect;
+import eu.mshade.mwork.event.EventContainer;
+import eu.mshade.mwork.event.EventListener;
 
-public class HandshakeListener implements DispatcherListener<PacketHandshakeEvent> {
+public class PacketHandshakeListener implements EventListener<PacketHandshakeEvent> {
 
-    private ProtocolRepository protocolRepository;
-    public HandshakeListener(ProtocolRepository protocolRepository) {
-        this.protocolRepository = protocolRepository;
+    private final DedicatedEnderChest dedicatedEnderChest;
+
+    public PacketHandshakeListener(DedicatedEnderChest dedicatedEnderChest) {
+        this.dedicatedEnderChest = dedicatedEnderChest;
     }
-
+    /*
     @Override
     public void handle(PacketHandshakeEvent packetHandshakeEvent, DispatcherContainer dispatcherContainer) {
         EnderFrameSessionHandler enderFrameSessionHandler = dispatcherContainer.getContainer(EnderFrameSessionHandler.class);
@@ -26,7 +29,38 @@ public class HandshakeListener implements DispatcherListener<PacketHandshakeEven
         }else {
             enderFrameSessionHandler.toggleProtocolStatus(ProtocolStatus.LOGIN);
             if (protocolVersion != ProtocolVersion.UNKNOWN) {
-                protocolRepository.getProtocolFrameByVersion(protocolVersion).ifPresent(enderFrameSessionHandler::toggleEnderFrameProtocol);
+                enderFrameSessionHandler.setProtocolVersion(protocolVersion);
+                dedicatedEnderChest.getProtocolRepository().getProtocolFrameByVersion(protocolVersion)
+                        .ifPresent(enderFrameSessionHandler::toggleEnderFrameProtocol)
+                        .ifNotPresent(unused -> {
+                            enderFrameSessionHandler.sendPacketAndClose(new PacketOutDisconnect(new TextComponent("You version has not supported by MShade")));
+                        });
+            }else {
+                enderFrameSessionHandler.sendPacketAndClose(new PacketOutDisconnect(new TextComponent("You version has not supported by MShade")));
+            }
+        }
+    }
+
+     */
+
+    @Override
+    public void onEvent(PacketHandshakeEvent event, EventContainer eventContainer) {
+        EnderFrameSessionHandler enderFrameSessionHandler = eventContainer.getContainer(EnderFrameSessionHandler.class);
+        HandshakeStatus handshakeStatus = event.getHandshake().getHandshakeStatus();
+        ProtocolVersion protocolVersion = event.getHandshake().getVersion();
+        if (handshakeStatus == HandshakeStatus.STATUS){
+            enderFrameSessionHandler.toggleProtocolStatus(ProtocolStatus.STATUS);
+        }else {
+            enderFrameSessionHandler.toggleProtocolStatus(ProtocolStatus.LOGIN);
+            if (protocolVersion != ProtocolVersion.UNKNOWN) {
+                enderFrameSessionHandler.setProtocolVersion(protocolVersion);
+                dedicatedEnderChest.getProtocolRepository().getProtocolFrameByVersion(protocolVersion)
+                        .ifPresent(enderFrameSessionHandler::toggleEnderFrameProtocol)
+                        .ifNotPresent(unused -> {
+                            enderFrameSessionHandler.sendPacketAndClose(new PacketOutDisconnect(TextComponent.of("You version has not supported by MShade")));
+                        });
+            }else {
+                enderFrameSessionHandler.sendPacketAndClose(new PacketOutDisconnect(TextComponent.of("You version has not supported by MShade")));
             }
         }
     }
