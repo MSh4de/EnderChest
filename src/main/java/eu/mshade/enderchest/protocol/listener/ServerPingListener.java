0bfd164754957833ef90f83dcc3f188fcded6159
RealAlphaUA <realalpha.ua@gmail.com>
2021-07-19 21:48:18 +0200

    update to EventListener 🎨

--- src/main/java/eu/mshade/enderchest/protocol/listener/ServerPingListener.java
+++ src/main/java/eu/mshade/enderchest/protocol/listener/ServerPingListener.java
@@ -1,16 +1,25 @@
-package eu.mshade.enderchest.listener;
+package eu.mshade.enderchest.protocol.listener;
 
 import eu.mshade.enderframe.EnderFrameSessionHandler;
 import eu.mshade.enderframe.event.server.ServerPingEvent;
-import eu.mshade.enderframe.protocol.packet.PacketOutPong;
-import eu.mshade.mwork.dispatcher.DispatcherContainer;
-import eu.mshade.mwork.dispatcher.DispatcherListener;
+import eu.mshade.enderframe.protocol.temp.packet.PacketOutPong;
+import eu.mshade.mwork.event.EventContainer;
+import eu.mshade.mwork.event.EventListener;
 
-public class ServerPingListener implements DispatcherListener<ServerPingEvent> {
+public class ServerPingListener implements EventListener<ServerPingEvent> {
 
+    /*
     @Override
     public void handle(ServerPingEvent serverPingEvent, DispatcherContainer dispatcherContainer) {
         EnderFrameSessionHandler bridge = dispatcherContainer.getContainer(EnderFrameSessionHandler.class);
         bridge.sendPacketAndClose(new PacketOutPong(serverPingEvent.getPayload()));
+    }
+
+     */
+
+    @Override
+    public void onEvent(ServerPingEvent event, EventContainer eventContainer) {
+        EnderFrameSessionHandler bridge = eventContainer.getContainer(EnderFrameSessionHandler.class);
+        bridge.sendPacketAndClose(new PacketOutPong(event.getPayload()));
     }
 }
