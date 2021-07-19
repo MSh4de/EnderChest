7b1764cda2b41c308d30949cdceb05d1b38d0bd6
RealAlphaUA <realalpha.ua@gmail.com>
2021-07-19 21:52:59 +0200

    add LegacyPingHandler 🥏

---
+++ src/main/java/eu/mshade/enderchest/protocol/LegacyPingHandler.java
@@ -0,0 +1,112 @@
+package eu.mshade.enderchest.protocol;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.io.UnsupportedEncodingException;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+
+public class LegacyPingHandler extends ChannelInboundHandlerAdapter {
+
+
+    @Override
+    public void channelRead(ChannelHandlerContext channelHandlerContext, Object object) {
+        ByteBuf bytebuf = (ByteBuf) object;
+
+        bytebuf.markReaderIndex();
+        boolean legacyPingProtocol = false;
+
+        try {
+            if (bytebuf.readByte() == (byte) 0xFE) {
+                int readableBytes = bytebuf.readableBytes();
+
+                InetSocketAddress inetsocketaddress = (InetSocketAddress) channelHandlerContext
+                        .channel().remoteAddress();
+
+                String motd = "COUCOU";
+                int player = 0;
+                int max = 20;
+                int protocol = 0;
+                String name = "";
+
+
+                switch (readableBytes) {
+                    case 0:
+                        sendByteBuf(channelHandlerContext, responseToByteBuf(channelHandlerContext,
+                                String.format("%s§%d§%d", motd,
+                                        player, max)));
+                        legacyPingProtocol = true;
+                        break;
+                    case 1:
+                        if (bytebuf.readByte() == (byte) 0x01) {
+                            sendByteBuf(channelHandlerContext,
+                                    responseToByteBuf(channelHandlerContext, String
+                                            .format("§1\0%d\0%s\0%s\0%d\0%d",
+                                                    protocol, name,
+                                                    motd,
+                                                    player,
+                                                    max)));
+                            legacyPingProtocol = true;
+                        }
+                        break;
+                    default:
+                        if (bytebuf.readByte() == (byte) 0x01 && bytebuf.readByte() == (byte) 0xFA
+                                && "MC|PingHost".equals(
+                                new String(bytebuf.readBytes(bytebuf.readShort() << 1).array(),
+                                        StandardCharsets.UTF_16BE))) {
+                            int dataLength = bytebuf.readUnsignedShort();
+                            short clientVersion = bytebuf.readUnsignedByte();
+                            String hostname = bytebuf.readBytes(bytebuf.readShort() << 1)
+                                    .toString(StandardCharsets.UTF_16BE);
+                            @SuppressWarnings("unused")
+                            int port = bytebuf.readInt();
+
+                            if (clientVersion >= 73 && 7 + (hostname.length() << 1) == dataLength
+                                    && bytebuf.readableBytes() == 0) {
+                                sendByteBuf(channelHandlerContext,
+                                        responseToByteBuf(channelHandlerContext, String
+                                                .format("§1\0%d\0%s\0%s\0%d\0%d",
+                                                        protocol, name,
+                                                        motd,
+                                                        player,
+                                                        max)));
+                                legacyPingProtocol = true;
+                            }
+                        }
+                        break;
+                }
+            }
+        } catch (RuntimeException expected) {
+            // Silently catch the exception
+        } finally {
+            // check if not successful, otherwise the connection has already been closed
+            if (!legacyPingProtocol) {
+                bytebuf.resetReaderIndex();
+                channelHandlerContext.pipeline().remove("legacy_ping");
+                channelHandlerContext.fireChannelRead(object);
+            }
+        }
+    }
+
+    private void sendByteBuf(ChannelHandlerContext channelhandlercontext, ByteBuf bytebuf) {
+        channelhandlercontext.writeAndFlush(bytebuf).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    private ByteBuf responseToByteBuf(ChannelHandlerContext ctx, String string) {
+        ByteBuf bytebuf = ctx.alloc().buffer(3 + string.length());
+
+        bytebuf.writeByte(0xFF);
+        bytebuf.writeShort(string.length());
+        try {
+            bytebuf.writeBytes(string.getBytes("UTF-16BE"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return bytebuf;
+    }
+
+}
