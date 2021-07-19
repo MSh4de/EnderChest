f8a75d3b77ec7ea6e1de859006be0366030cc8e6
RealAlphaUA <realalpha.ua@gmail.com>
2021-07-19 21:53:28 +0200

    add PacketLoginHandler 🎲

---
+++ src/main/java/eu/mshade/enderchest/protocol/listener/PacketLoginHandler.java
@@ -0,0 +1,38 @@
+package eu.mshade.enderchest.protocol.listener;
+
+import eu.mshade.enderchest.DedicatedEnderChest;
+import eu.mshade.enderframe.EnderFrameSession;
+import eu.mshade.enderframe.EnderFrameSessionHandler;
+import eu.mshade.enderframe.event.entity.PacketLoginEvent;
+import eu.mshade.enderframe.mojang.GameProfile;
+import eu.mshade.mwork.event.EventContainer;
+import eu.mshade.mwork.event.EventListener;
+
+public class PacketLoginHandler implements EventListener<PacketLoginEvent> {
+
+    private DedicatedEnderChest dedicatedEnderChest;
+
+    public PacketLoginHandler(DedicatedEnderChest dedicatedEnderChest) {
+        this.dedicatedEnderChest = dedicatedEnderChest;
+    }
+    /*
+    @Override
+    public void handle(PacketLoginEvent packetLoginEvent, DispatcherContainer dispatcherContainer) {
+        EnderFrameSessionHandler enderFrameSessionHandler = dispatcherContainer.getContainer(EnderFrameSessionHandler.class);
+        EnderFrameSession enderFrameSession = enderFrameSessionHandler.getEnderFrameProtocol().getEnderFrameSession(enderFrameSessionHandler);
+        enderFrameSessionHandler.setEnderFrameSession(enderFrameSession);
+        enderFrameSession.setGameProfile(new GameProfile(packetLoginEvent.getName()));
+        enderFrameSession.sendEncryption(dedicatedEnderChest.getMinecraftEncryption().getKeyPair().getPublic());
+    }
+
+     */
+
+    @Override
+    public void onEvent(PacketLoginEvent event, EventContainer eventContainer) {
+        EnderFrameSessionHandler enderFrameSessionHandler = eventContainer.getContainer(EnderFrameSessionHandler.class);
+        EnderFrameSession enderFrameSession = enderFrameSessionHandler.getEnderFrameProtocol().getEnderFrameSession(enderFrameSessionHandler);
+        enderFrameSessionHandler.setEnderFrameSession(enderFrameSession);
+        enderFrameSession.setGameProfile(new GameProfile(event.getName()));
+        enderFrameSession.sendEncryption(dedicatedEnderChest.getMinecraftEncryption().getKeyPair().getPublic());
+    }
+}
