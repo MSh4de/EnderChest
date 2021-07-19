efae83beedb03c02a3effc302b64c02cca033ef6
RealAlphaUA <realalpha.ua@gmail.com>
2021-07-19 21:48:35 +0200

    update ServerStatusListener 🎠

--- src/main/java/eu/mshade/enderchest/protocol/listener/ServerStatusListener.java
+++ src/main/java/eu/mshade/enderchest/protocol/listener/ServerStatusListener.java
@@ -1,32 +1,58 @@
-package eu.mshade.enderchest.listener;
+package eu.mshade.enderchest.protocol.listener;
 
 import eu.mshade.enderframe.EnderFrameSessionHandler;
 import eu.mshade.enderframe.ServerListBuilder;
 import eu.mshade.enderframe.ServerListPlayerBuilder;
 import eu.mshade.enderframe.event.server.ServerStatusEvent;
-import eu.mshade.enderframe.protocol.packet.PacketOutStatus;
-import eu.mshade.mwork.dispatcher.DispatcherContainer;
-import eu.mshade.mwork.dispatcher.DispatcherListener;
+import eu.mshade.enderframe.protocol.temp.packet.PacketOutStatus;
+import eu.mshade.mwork.event.EventContainer;
+import eu.mshade.mwork.event.EventListener;
 
-public class ServerStatusListener implements DispatcherListener<ServerStatusEvent> {
-
+public class ServerStatusListener implements EventListener<ServerStatusEvent> {
+    /*
     @Override
     public void handle(ServerStatusEvent serverStatusEvent, DispatcherContainer dispatcherContainer) {
         EnderFrameSessionHandler bridge = dispatcherContainer.getContainer(EnderFrameSessionHandler.class);
 
+        //System.out.println(ChatColor.BLACK+"Hello");
+
         ServerListBuilder serverListBuilder = ServerListBuilder.builder();
-        serverListBuilder.setVersion("EnderChest 1.8.X", 47);
+        serverListBuilder.setVersion("EnderChest 1.8.X §eMerci", 47);
 
         serverListBuilder.setPlayer(20, 0,
-                ServerListPlayerBuilder.of("Hello"),
-                ServerListPlayerBuilder.of("ce"),
-                ServerListPlayerBuilder.of("si"),
-                ServerListPlayerBuilder.of("est"),
-                ServerListPlayerBuilder.of("un"),
-                ServerListPlayerBuilder.of("test")
+                ServerListPlayerBuilder.of("§4Hello"),
+                ServerListPlayerBuilder.of("§cce"),
+                ServerListPlayerBuilder.of("§6si"),
+                ServerListPlayerBuilder.of("§eest"),
+                ServerListPlayerBuilder.of("§2un"),
+                ServerListPlayerBuilder.of("§atest")
         );
 
 
         bridge.sendPacket(new PacketOutStatus(serverListBuilder));
+    }
+
+     */
+
+    @Override
+    public void onEvent(ServerStatusEvent event, EventContainer eventContainer) {
+        EnderFrameSessionHandler enderFrameSessionHandler = eventContainer.getContainer(EnderFrameSessionHandler.class);
+
+        //System.out.println(ChatColor.BLACK+"Hello");
+
+        ServerListBuilder serverListBuilder = ServerListBuilder.builder();
+        serverListBuilder.setVersion("EnderChest 1.8.X §eMerci", 47);
+
+        serverListBuilder.setPlayer(20, 0,
+                ServerListPlayerBuilder.of("§4Hello"),
+                ServerListPlayerBuilder.of("§cce"),
+                ServerListPlayerBuilder.of("§6si"),
+                ServerListPlayerBuilder.of("§eest"),
+                ServerListPlayerBuilder.of("§2un"),
+                ServerListPlayerBuilder.of("§atest")
+        );
+
+
+        enderFrameSessionHandler.sendPacket(new PacketOutStatus(serverListBuilder));
     }
 }
