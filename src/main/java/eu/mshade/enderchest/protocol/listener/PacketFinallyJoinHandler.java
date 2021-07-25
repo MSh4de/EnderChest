98880e969c358be0afe90e0f71964333202b4c25
NewZ_AZ <BenjaminDeveloppement@gmail.com>
2021-07-25 11:52:49 +0200

    Update PacketFinallyJoinHandler

--- src/main/java/eu/mshade/enderchest/protocol/listener/PacketFinallyJoinHandler.java
+++ src/main/java/eu/mshade/enderchest/protocol/listener/PacketFinallyJoinHandler.java
@@ -2,14 +2,21 @@ package eu.mshade.enderchest.protocol.listener;
 
 import eu.mshade.enderchest.DedicatedEnderChest;
 import eu.mshade.enderframe.*;
+import eu.mshade.enderframe.entity.EntityType;
 import eu.mshade.enderframe.event.entity.PacketFinallyJoinEvent;
+import eu.mshade.enderframe.metadata.attribute.AttributeProperty;
+import eu.mshade.enderframe.metadata.attribute.AttributeType;
 import eu.mshade.enderframe.world.Location;
+import eu.mshade.enderframe.world.Position;
 import eu.mshade.enderframe.world.WorldBuffer;
-import eu.mshade.enderman.packet.play.PacketOutSpawnMob;
+import eu.mshade.enderman.packet.play.*;
 import eu.mshade.mwork.event.EventContainer;
 import eu.mshade.mwork.event.EventListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PacketFinallyJoinHandler implements EventListener<PacketFinallyJoinEvent> {
 
@@ -38,26 +45,38 @@ public class PacketFinallyJoinHandler implements EventListener<PacketFinallyJoin
         enderFrameSession.sendSquareChunk(10, location.getChunkX(), location.getChunkZ(), world);
         enderFrameSession.sendPosition(location);
         enderFrameSession.sendAbilities(false, false, true, false, 0.5F, 0.2F);
-
-        enderFrameSessionHandler.sendPacket(new PacketOutSpawnMob());
+        List<AttributeProperty> attributeProperties = new ArrayList<>();
+        for (AttributeType value : AttributeType.values()) {
+            attributeProperties.add(new AttributeProperty(value, value.getMax()));
+        }
 
         dedicatedEnderChest.addPlayer(enderFrameSession);
 
         PlayerInfoBuilder playerInfoBuilder = PlayerInfoBuilder.of(PlayerInfoType.ADD_PLAYER);
         dedicatedEnderChest.getEnderFrameSessions().forEach(playerInfoBuilder::withPlayer);
-        dedicatedEnderChest.getEnderFrameSessions().forEach(target -> target.sendPlayerInfo(playerInfoBuilder));
-
-        /*
-        dedicatedEnderChest.getEnderFrameSessions().forEach(target -> {
-            if (target != enderFrameSession)
-            target.getEnderFrameSessionHandler().sendPacket(new PacketOutSpawnPlayer(1, enderFrameSession.getGameProfile().getId(), new Position(0, 112, 0, false)));
+        dedicatedEnderChest.getEnderFrameSessions().forEach(target ->{
+            target.sendPlayerInfo(playerInfoBuilder);
+            target.sendMessage(String.format("%s join server", enderFrameSession.getGameProfile().getName()));
         });
 
-         */
+
+        dedicatedEnderChest.getEnderFrameSessions().forEach(target -> {
+           if (target != enderFrameSession) target.getEnderFrameSessionHandler().sendPacket(new PacketOutSpawnPlayer(8, enderFrameSession.getGameProfile().getId(), new Position(0, highest+1, 0, false)));
+
+        });
+
+
         logger.info(String.format("%s join server", enderFrameSession.getGameProfile().getName()));
 
         enderFrameSession.sendMessage("Welcome to project MShade");
         enderFrameSession.sendPlayerList("Hey this is test", "and this is test");
-
+        enderFrameSessionHandler.sendPacket(new PacketOutSpawnEntity(2));
+        PacketOutSpawnMob packetOutSpawnMob = new PacketOutSpawnMob(2,54, 7, 52, 7);
+        enderFrameSessionHandler.sendPacket(packetOutSpawnMob);
+        /*enderFrameSessionHandler.sendPacket(new PacketOutEntityHeadLook(2,0));
+        enderFrameSessionHandler.sendPacket(new PacketOutEntityMetadata(2, EntityType.ZOMBIE, packetOutSpawnMob.get()));
+        enderFrameSessionHandler.sendPacket(new PacketOutEntityTeleport(2,7*32,53*32,7*32,(byte) 0,(byte) 0,false));
+        enderFrameSessionHandler.sendPacket(new PacketOutEntityProperties(2, attributeProperties));
+        enderFrameSessionHandler.sendPacket(new PacketOutSpawnPlayer(3, enderFrameSession.getGameProfile().getId(), new Position(0, highest+1, 0, false)));*/
     }
 }
