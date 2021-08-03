5248cee325aacea3581a5cf8ce46b1e3deef8193
RealAlphaUA <realalpha.ua@gmail.com>
2021-08-03 23:54:37 +0200

    change ServerListBuilder to MotdComponent 🔮

--- src/main/java/eu/mshade/enderchest/protocol/listener/ServerStatusListener.java
+++ src/main/java/eu/mshade/enderchest/protocol/listener/ServerStatusListener.java
@@ -1,58 +1,47 @@
 package eu.mshade.enderchest.protocol.listener;
 
+import eu.mshade.enderchest.redstone.Redstone;
+import eu.mshade.enderchest.redstone.RedstoneSession;
+import eu.mshade.enderchest.redstone.protocol.packet.RedstonePacketInMotd;
+import eu.mshade.enderchest.redstone.protocol.packet.RedstonePacketOutMotd;
 import eu.mshade.enderframe.EnderFrameSessionHandler;
-import eu.mshade.enderframe.ServerListBuilder;
-import eu.mshade.enderframe.ServerListPlayerBuilder;
 import eu.mshade.enderframe.event.server.ServerStatusEvent;
+import eu.mshade.enderframe.mojang.chat.TextComponent;
+import eu.mshade.enderframe.motd.MotdComponent;
+import eu.mshade.enderframe.motd.MotdPlayer;
+import eu.mshade.enderframe.motd.MotdVersion;
 import eu.mshade.enderframe.protocol.temp.packet.PacketOutStatus;
 import eu.mshade.mwork.event.EventContainer;
 import eu.mshade.mwork.event.EventListener;
 
+import java.util.concurrent.CompletableFuture;
+
 public class ServerStatusListener implements EventListener<ServerStatusEvent> {
-    /*
-    @Override
-    public void handle(ServerStatusEvent serverStatusEvent, DispatcherContainer dispatcherContainer) {
-        EnderFrameSessionHandler bridge = dispatcherContainer.getContainer(EnderFrameSessionHandler.class);
 
-        //System.out.println(ChatColor.BLACK+"Hello");
-
-        ServerListBuilder serverListBuilder = ServerListBuilder.builder();
-        serverListBuilder.setVersion("EnderChest 1.8.X §eMerci", 47);
-
-        serverListBuilder.setPlayer(20, 0,
-                ServerListPlayerBuilder.of("§4Hello"),
-                ServerListPlayerBuilder.of("§cce"),
-                ServerListPlayerBuilder.of("§6si"),
-                ServerListPlayerBuilder.of("§eest"),
-                ServerListPlayerBuilder.of("§2un"),
-                ServerListPlayerBuilder.of("§atest")
-        );
-
-
-        bridge.sendPacket(new PacketOutStatus(serverListBuilder));
+    private Redstone redstone;
+    public ServerStatusListener(Redstone redstone) {
+        this.redstone = redstone;
     }
-
-     */
 
     @Override
     public void onEvent(ServerStatusEvent event, EventContainer eventContainer) {
         EnderFrameSessionHandler enderFrameSessionHandler = eventContainer.getContainer(EnderFrameSessionHandler.class);
 
-        //System.out.println(ChatColor.BLACK+"Hello");
-
-        ServerListBuilder serverListBuilder = ServerListBuilder.builder();
-        serverListBuilder.setVersion("EnderChest 1.8.X §eMerci", 47);
-
-        serverListBuilder.setPlayer(20, 0,
-                ServerListPlayerBuilder.of("§4Hello"),
-                ServerListPlayerBuilder.of("§cce"),
-                ServerListPlayerBuilder.of("§6si"),
-                ServerListPlayerBuilder.of("§eest"),
-                ServerListPlayerBuilder.of("§2un"),
-                ServerListPlayerBuilder.of("§atest")
-        );
+        RedstonePacketOutMotd redstonePacketOutMotd = new RedstonePacketOutMotd();
+        MotdVersion motdVersion = new MotdVersion("1.8.X", 47);
+        MotdPlayer motdPlayer = new MotdPlayer(20, 0);
+        MotdComponent motdComponent = new MotdComponent(motdVersion, motdPlayer, TextComponent.of("Hello"));
+        CompletableFuture<RedstonePacketInMotd> redstonePacketInMotdCompletableFuture = CompletableFuture.completedFuture(new RedstonePacketInMotd(motdComponent));
 
 
-        enderFrameSessionHandler.sendPacket(new PacketOutStatus(serverListBuilder));
+        for (RedstoneSession redstoneSession : redstone.getRedstoneSessions()) {
+            System.out.println(redstoneSession);
+            redstonePacketInMotdCompletableFuture = redstone.sendPacket(redstoneSession, redstonePacketOutMotd, RedstonePacketInMotd.class);
+        }
+        redstonePacketInMotdCompletableFuture.thenAccept(redstonePacketInMotd -> {
+            System.out.println(redstonePacketInMotd);
+            enderFrameSessionHandler.sendPacket(new PacketOutStatus(redstonePacketInMotd.getMotdComponent()));
+        });
+
     }
 }
