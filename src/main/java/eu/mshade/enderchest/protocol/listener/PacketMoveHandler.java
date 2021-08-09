6c29008c79c6b414622d7f5c2c3d22ef9fa9345f
NewZ_AZ <BenjaminDeveloppement@gmail.com>
2021-08-09 12:23:32 +0200

    fixed movement

--- src/main/java/eu/mshade/enderchest/protocol/listener/PacketMoveHandler.java
+++ src/main/java/eu/mshade/enderchest/protocol/listener/PacketMoveHandler.java
@@ -15,8 +15,12 @@ import eu.mshade.mwork.event.EventListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 public class PacketMoveHandler implements EventListener<PacketMoveEvent> {
 
+    public static final HashMap<EnderFrameSession, Integer> linkedMob = new LinkedHashMap<>();
     private static final Logger LOGGER = LoggerFactory.getLogger(PacketMoveHandler.class);
     private DedicatedEnderChest dedicatedEnderChest;
 
@@ -56,48 +60,52 @@ public class PacketMoveHandler implements EventListener<PacketMoveEvent> {
 
         if (before.getChunkX() != now.getChunkX() || before.getChunkZ() != now.getChunkZ()) {
             enderFrameSession.sendSquareChunk(10, now.getChunkX(), now.getChunkZ(), now.getWorld());
-
         }
 
         Player player = world.getPlayer(enderFrameSessionHandler);
-        int x1 = (int) (now.getX() * 32);
-        int x2 = (int) (before.getX() * 32);
-        int y1 = (int) (now.getY() * 32);
-        int y2 = (int) (before.getY() * 32);
-        int z1 = (int) (now.getZ() * 32);
-        int z2 = (int) (before.getZ() * 32);
+        int x1 = floor(now.getX() * 32);
+        int x2 = floor(before.getX() * 32);
+        int y1 = floor(now.getY() * 32);
+        int y2 = floor(before.getY() * 32);
+        int z1 = floor(now.getZ() * 32);
+        int z2 = floor(before.getZ() * 32);
 
-        int deltaX = x1 - x2;
-        int deltaY = y1 - y2;
-        int deltaZ = z1 - z2;
-        byte yaw1 = (byte) (now.getYaw() % 360 / 360 * 256);
-        byte pitch1 = (byte) (now.getPitch() % 360 / 360 * 256);
-
+        int deltaX = (x1 - x2);
+        int deltaY = (y1 - y2);
+        int deltaZ = (z1 - z2);
+        int yaw1 = (int) (now.getYaw() % 360 / 360 * 256);
+        int pitch1 = (int) (now.getPitch() % 360 / 360 * 256);
         boolean teleport = hasOverflow(deltaX) || hasOverflow(deltaY) || hasOverflow(deltaZ);
-        if (!teleport) {
-            if (event.getPacketMoveType() == PacketMoveType.LOOK) {
+
+
+            if (event.getPacketMoveType() == PacketMoveType.LOOK || event.getPacketMoveType() == PacketMoveType.POSITION_AND_LOOK) {
                 player.getViewers().forEach(target -> {
-                    target.getEnderFrameSessionHandler().sendPacket(new PacketOutEntityLook(now.getWorld().getPlayer(enderFrameSessionHandler).getEntityId(), yaw1, pitch1, position.isGround()));
-                    target.getEnderFrameSessionHandler().sendPacket(new PacketOutEntityHeadLook(now.getWorld().getPlayer(enderFrameSessionHandler).getEntityId(), yaw1));
+                    target.getEnderFrameSessionHandler().sendPacket(new PacketOutEntityLook(player.getEntityId(), yaw1, pitch1, position.isGround()));
+                    target.getEnderFrameSessionHandler().sendPacket(new PacketOutEntityHeadLook(player.getEntityId(), yaw1));
                 });
             }
+        if (!teleport) {
             if (event.getPacketMoveType() == PacketMoveType.POSITION_AND_LOOK) {
                 player.getViewers().forEach(target -> {
-                    target.getEnderFrameSessionHandler().sendPacket(new PacketOutEntityLookRelativeMove(now.getWorld().getPlayer(enderFrameSessionHandler).getEntityId(), deltaX, deltaY, deltaZ, yaw1, pitch1, position.isGround()));
-                    target.getEnderFrameSessionHandler().sendPacket(new PacketOutEntityHeadLook(now.getWorld().getPlayer(enderFrameSessionHandler).getEntityId(), yaw1));
+                    target.getEnderFrameSessionHandler().sendPacket(new PacketOutEntityLookRelativeMove(player.getEntityId(), (byte) deltaX, (byte) deltaY, (byte) deltaZ, yaw1, pitch1, position.isGround()));
                 });
             } else if (event.getPacketMoveType().equals(PacketMoveType.POSITION)) {
-                player.getViewers().forEach(target -> target.getEnderFrameSessionHandler().sendPacket(new PacketOutEntityRelativeMove(now.getWorld().getPlayer(enderFrameSessionHandler).getEntityId(), deltaX, deltaY, deltaZ, position.isGround())));
+                player.getViewers().forEach(target -> target.getEnderFrameSessionHandler().sendPacket(new PacketOutEntityRelativeMove(player.getEntityId(), (byte) deltaX, (byte) deltaY, (byte) deltaZ, position.isGround())));
             }
         } else {
-            player.setLocation(now);
-            player.getViewers().forEach(target -> target.getEnderFrameSessionHandler().sendPacket(new PacketOutEntityTeleport(player, position.isGround())));
+            player.getViewers().forEach(target ->{
+                target.getEnderFrameSessionHandler().sendPacket(new PacketOutEntityTeleport(player.getEntityId(), player, position.isGround()));
+            });
         }
-
-
     }
 
     public boolean hasOverflow(int value) {
-        return value > Byte.MAX_VALUE || value < Byte.MIN_VALUE;
+        return value > 3 || value < -3;
+    }
+
+    public int floor(double d0) {
+        int i = (int) d0;
+
+        return d0 < (double) i ? i - 1 : i;
     }
 }
