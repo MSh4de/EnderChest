97dc284fd98f412789eb53dd80b05a6649200a58
Zentae <yanistahiri560@gmail.com>
2021-08-12 03:50:43 +0200

    added marshals. 🎆

---
+++ src/main/java/eu/mshade/enderchest/marshals/assets/DefaultAgeableMarshal.java
@@ -0,0 +1,28 @@
+package eu.mshade.enderchest.marshals;
+
+import eu.mshade.enderchest.entity.DefaultAgeableEntity;
+import eu.mshade.enderframe.entity.Ageable;
+import eu.mshade.mwork.ParameterContainer;
+import eu.mshade.mwork.binarytag.BinaryTag;
+import eu.mshade.mwork.binarytag.entity.CompoundBinaryTag;
+import eu.mshade.mwork.binarytag.marshal.BinaryTagMarshal;
+import eu.mshade.mwork.binarytag.marshal.BinaryTagMarshalBuffer;
+
+import java.lang.reflect.Type;
+
+public class DefaultAgeableMarshal implements BinaryTagMarshalBuffer<Ageable> {
+
+    @Override
+    public BinaryTag<?> serialize(BinaryTagMarshal binaryTagMarshal, Type type, Ageable ageable, ParameterContainer parameterContainer) throws Exception {
+        CompoundBinaryTag compoundBinaryTag = new CompoundBinaryTag();
+        compoundBinaryTag.putInt("age", ageable.getAge());
+
+        return compoundBinaryTag;
+    }
+
+    @Override
+    public Ageable deserialize(BinaryTagMarshal binaryTagMarshal, Type type, BinaryTag<?> binaryTag, ParameterContainer parameterContainer) throws Exception {
+        CompoundBinaryTag compoundBinaryTag = (CompoundBinaryTag) binaryTag;
+        return new DefaultAgeableEntity(compoundBinaryTag.getInt("age"));
+    }
+}
