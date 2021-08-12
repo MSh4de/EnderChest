cd840181a5ced2243f49d63d7fc0d4a0840458ca
Zentae <yanistahiri560@gmail.com>
2021-08-12 03:57:10 +0200

    Refactoring. 🧨

---
+++ src/main/java/eu/mshade/enderchest/marshals/entity/DefaultPlayerMarshal.java
@@ -0,0 +1,66 @@
+package eu.mshade.enderchest.marshals.entity;
+
+import eu.mshade.enderchest.entity.DefaultPlayer;
+import eu.mshade.enderframe.EnderFrameSession;
+import eu.mshade.enderframe.EnderFrameSessionHandler;
+import eu.mshade.enderframe.entity.Entity;
+import eu.mshade.enderframe.entity.LivingEntity;
+import eu.mshade.enderframe.entity.Player;
+import eu.mshade.enderframe.mojang.SkinParts;
+import eu.mshade.mwork.ParameterContainer;
+import eu.mshade.mwork.binarytag.BinaryTag;
+import eu.mshade.mwork.binarytag.entity.CompoundBinaryTag;
+import eu.mshade.mwork.binarytag.marshal.BinaryTagMarshal;
+
+import java.lang.reflect.Type;
+
+public class DefaultPlayerMarshal extends DefaultLivingEntityMarshal {
+
+    @Override
+    public BinaryTag<?> serialize(BinaryTagMarshal binaryTagMarshal, Type type, Entity entity, ParameterContainer parameterContainer) throws Exception {
+        CompoundBinaryTag compoundBinaryTag = (CompoundBinaryTag) super.serialize(binaryTagMarshal, type, entity, parameterContainer);
+        Player player = (Player)entity;
+
+        compoundBinaryTag.putString("name", player.getName());
+        compoundBinaryTag.putFloat("absorptionHearts", player.getAbsorptionHearts());
+        compoundBinaryTag.putInt("score", player.getScore());
+
+        return compoundBinaryTag;
+    }
+
+    @Override
+    public Player deserialize(BinaryTagMarshal binaryTagMarshal, Type type, BinaryTag<?> binaryTag, ParameterContainer parameterContainer) throws Exception {
+        LivingEntity livingEntity = super.deserialize(binaryTagMarshal, type, binaryTag, parameterContainer);
+        EnderFrameSessionHandler enderFrameSessionHandler = parameterContainer.getContainer(EnderFrameSessionHandler.class);
+        EnderFrameSession enderFrameSession = enderFrameSessionHandler.getEnderFrameSession();
+        CompoundBinaryTag compoundBinaryTag = (CompoundBinaryTag) binaryTag;
+
+        return new DefaultPlayer(livingEntity.getLocation(),
+                livingEntity.getVelocity(),
+                livingEntity.getEntityId(),
+                livingEntity.isFire(),
+                livingEntity.isSneaking(),
+                livingEntity.isSprinting(),
+                livingEntity.isEating(),
+                livingEntity.isInvisible(),
+                livingEntity.getAirTicks(),
+                livingEntity.getCustomName(),
+                livingEntity.isCustomNameVisible(),
+                livingEntity.isSilent(),
+                livingEntity.getUUID(),
+                livingEntity.getHealth(),
+                livingEntity.getPotionEffectColor(),
+                livingEntity.isPotionEffectAmbient(),
+                livingEntity.getNumberOfArrowInEntity(),
+                livingEntity.isAIDisable(),
+                enderFrameSessionHandler,
+                compoundBinaryTag.getString("name"),
+                enderFrameSession.getSocketAddress(),
+                enderFrameSessionHandler.getProtocolVersion(),
+                0,
+                SkinParts.fromByte((byte)127),
+                false,
+                compoundBinaryTag.getFloat("absorptionHeart"),
+                compoundBinaryTag.getInt("score"));
+    }
+}
---
+++ src/main/java/eu/mshade/enderchest/marshals/world/DefaultLocationMarshal.java
@@ -0,0 +1,44 @@
+package eu.mshade.enderchest.marshals.world;
+
+import eu.mshade.enderchest.world.WorldManager;
+import eu.mshade.enderframe.world.Location;
+import eu.mshade.enderframe.world.WorldBuffer;
+import eu.mshade.mwork.ParameterContainer;
+import eu.mshade.mwork.binarytag.BinaryTag;
+import eu.mshade.mwork.binarytag.entity.CompoundBinaryTag;
+import eu.mshade.mwork.binarytag.marshal.BinaryTagMarshal;
+import eu.mshade.mwork.binarytag.marshal.BinaryTagMarshalBuffer;
+
+import java.lang.reflect.Type;
+
+public class DefaultLocationMarshal implements BinaryTagMarshalBuffer<Location> {
+
+    @Override
+    public BinaryTag<?> serialize(BinaryTagMarshal binaryTagMarshal, Type type, Location location, ParameterContainer parameterContainer) throws Exception {
+        CompoundBinaryTag compoundBinaryTag = new CompoundBinaryTag();
+
+        compoundBinaryTag.putString("worldName", location.getWorld().getWorldLevel().getName());
+        compoundBinaryTag.putDouble("x", location.getX());
+        compoundBinaryTag.putDouble("y", location.getY());
+        compoundBinaryTag.putDouble("z", location.getZ());
+        compoundBinaryTag.putFloat("yaw", location.getYaw());
+        compoundBinaryTag.putFloat("pitch", location.getPitch());
+
+        return compoundBinaryTag;
+    }
+
+    @Override
+    public Location deserialize(BinaryTagMarshal binaryTagMarshal, Type type, BinaryTag<?> binaryTag, ParameterContainer parameterContainer) throws Exception {
+        WorldManager worldManager = parameterContainer.getContainer(WorldManager.class);
+        CompoundBinaryTag compoundBinaryTag = (CompoundBinaryTag)binaryTag;
+
+        WorldBuffer world = worldManager.getWorldBuffer(compoundBinaryTag.getString("worldName"));
+        double x = compoundBinaryTag.getDouble("x");
+        double y = compoundBinaryTag.getDouble("y");
+        double z = compoundBinaryTag.getDouble("z");
+        float yaw = compoundBinaryTag.getFloat("yaw");
+        float pitch = compoundBinaryTag.getFloat("pitch");
+
+        return new Location(world, x, y, z, yaw, pitch);
+    }
+}
