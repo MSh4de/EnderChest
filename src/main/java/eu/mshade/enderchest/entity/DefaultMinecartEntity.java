48945831cc29552a82de529e48781f51c8448d30
NewZAZ <ben.tintin.bp@gmail.com>
2021-08-22 10:34:51 +0200

    🚧 fixed constructor

--- src/main/java/eu/mshade/enderchest/entity/DefaultMinecartEntity.java
+++ src/main/java/eu/mshade/enderchest/entity/DefaultMinecartEntity.java
@@ -9,24 +9,24 @@ import java.util.UUID;
 
 public class DefaultMinecartEntity extends Minecart {
 
-    public DefaultMinecartEntity(Location location, Vector velocity, int entityId, boolean isFire, boolean isSneaking, boolean isSprinting, boolean isEating, boolean isInvisible, short airTicks, String customName, boolean isCustomNameVisible, boolean isSilent, UUID uuid, EntityType entityType, int shakingPower, int shakingDirection, int blockId, int blockData, int blockYPosition, boolean showBlock, float damageTaken, Vector vehicleVelocity) {
-        super(location, velocity, entityId, isFire, isSneaking, isSprinting, isEating, isInvisible, airTicks, customName, isCustomNameVisible, isSilent, uuid, entityType, shakingPower, shakingDirection, blockId, blockData, blockYPosition, showBlock, damageTaken, vehicleVelocity);
+    public DefaultMinecartEntity(Location location, Vector velocity, int entityId, boolean isFire, boolean isSneaking, boolean isSprinting, boolean isEating, boolean isInvisible, short airTicks, String customName, boolean isCustomNameVisible, boolean isSilent, UUID uuid, EntityType entityType, int shakingPower, int shakingDirection, int blockId, int blockData, int blockYPosition, boolean showBlock, float damageTaken) {
+        super(location, velocity, entityId, isFire, isSneaking, isSprinting, isEating, isInvisible, airTicks, customName, isCustomNameVisible, isSilent, uuid, entityType, shakingPower, shakingDirection, blockId, blockData, blockYPosition, showBlock, damageTaken);
     }
 
-    public DefaultMinecartEntity(Location location, int entityId, EntityType entityType, int shakingPower, int shakingDirection, int blockId, int blockData, int blockYPosition, boolean showBlock, float damageTaken, Vector vehicleVelocity) {
-        super(location, entityId, entityType, shakingPower, shakingDirection, blockId, blockData, blockYPosition, showBlock, damageTaken, vehicleVelocity);
+    public DefaultMinecartEntity(Location location, int entityId, EntityType entityType, int shakingPower, int shakingDirection, int blockId, int blockData, int blockYPosition, boolean showBlock, float damageTaken) {
+        super(location, entityId, entityType, shakingPower, shakingDirection, blockId, blockData, blockYPosition, showBlock, damageTaken);
     }
 
     public DefaultMinecartEntity(Location location, EntityType entityType, int entityId) {
         super(location, entityType, entityId);
     }
 
+    public DefaultMinecartEntity(Location location, int entityId) {
+        super(location, entityId);
+    }
+
     @Override
     public void tick() {
 
-    }
-
-    public DefaultMinecartEntity(Location location, int entityId) {
-        super(location, entityId);
     }
 }
