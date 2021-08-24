1d0cab4a31bdb9dd039002962ef7aef9ec16b8f0
Zentae <yanistahiri560@gmail.com>
2021-08-24 06:51:03 +0200

    add brand new entities implementations ✨

---
+++ src/main/java/eu/mshade/enderchest/entity/DefaultEnderDragonEntity.java
@@ -0,0 +1,27 @@
+package eu.mshade.enderchest.entity;
+
+import eu.mshade.enderframe.entity.EnderDragon;
+import eu.mshade.enderframe.world.Location;
+import eu.mshade.enderframe.world.Vector;
+
+import java.util.UUID;
+
+public class DefaultEnderDragonEntity extends EnderDragon {
+
+    public DefaultEnderDragonEntity(Location location, int entityId, float health) {
+        super(location, entityId, health);
+    }
+
+    public DefaultEnderDragonEntity(Location location, Vector velocity, int entityId, boolean isFire, boolean isSneaking, boolean isSprinting, boolean isEating, boolean isInvisible, short airTicks, String customName, boolean isCustomNameVisible, boolean isSilent, UUID uuid, float health, int potionEffectColor, boolean isPotionEffectAmbient, byte numberOfArrowInEntity, boolean isAIDisable) {
+        super(location, velocity, entityId, isFire, isSneaking, isSprinting, isEating, isInvisible, airTicks, customName, isCustomNameVisible, isSilent, uuid, health, potionEffectColor, isPotionEffectAmbient, numberOfArrowInEntity, isAIDisable);
+    }
+
+    public DefaultEnderDragonEntity(Location location, int entityId) {
+        super(location, entityId);
+    }
+
+    @Override
+    public void tick() {
+
+    }
+}
