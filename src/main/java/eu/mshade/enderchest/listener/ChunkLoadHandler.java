42b1eb7d5229d2fd270d0a571d47af59f47b43a3
NewZAZ <ben.tintin.bp@gmail.com>
2021-08-31 19:07:10 +0200

    Add ChunkLoadHandler 👂

---
+++ src/main/java/eu/mshade/enderchest/listener/ChunkLoadHandler.java
@@ -0,0 +1,15 @@
+package eu.mshade.enderchest.listener;
+
+import eu.mshade.enderframe.event.ChunkLoadEvent;
+import eu.mshade.enderframe.world.ChunkBuffer;
+import eu.mshade.mwork.ParameterContainer;
+import eu.mshade.mwork.event.EventListener;
+
+public class ChunkLoadHandler implements EventListener<ChunkLoadEvent> {
+
+    @Override
+    public void onEvent(ChunkLoadEvent event, ParameterContainer eventContainer) {
+        ChunkBuffer chunkBuffer = event.getChunkBuffer();
+
+    }
+}
