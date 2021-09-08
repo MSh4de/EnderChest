24cb8bc786350b3dfb56d342e4acaa20fc8f9117
NewZAZ <ben.tintin.bp@gmail.com>
2021-09-08 15:27:55 +0200

    Add WatchDog Handler ✨

---
+++ src/main/java/eu/mshade/enderchest/listener/WatchdogUnseeHandler.java
@@ -0,0 +1,13 @@
+package eu.mshade.enderchest.listener;
+
+import eu.mshade.enderframe.event.WatchdogUnseeEvent;
+import eu.mshade.mwork.ParameterContainer;
+import eu.mshade.mwork.event.EventListener;
+
+public class WatchdogUnseeHandler implements EventListener<WatchdogUnseeEvent> {
+
+    @Override
+    public void onEvent(WatchdogUnseeEvent event, ParameterContainer eventContainer) {
+
+    }
+}
