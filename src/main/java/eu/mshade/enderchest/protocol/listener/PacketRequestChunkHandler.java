d0f524ff9d79d7c9c50e3de289b9542d277ef623
RealAlphaUA <realalpha.ua@gmail.com>
2021-11-14 22:54:23 +0100

    update PacketRequestChunkHandler 🎊

--- src/main/java/eu/mshade/enderchest/protocol/listener/PacketRequestChunkHandler.java
+++ src/main/java/eu/mshade/enderchest/protocol/listener/PacketRequestChunkHandler.java
@@ -6,22 +6,18 @@ import eu.mshade.enderframe.EnderFrameSessionHandler;
 import eu.mshade.enderframe.entity.Player;
 import eu.mshade.enderframe.packetevent.PacketMoveEvent;
 import eu.mshade.enderframe.world.Location;
-import eu.mshade.enderframe.world.Position;
 import eu.mshade.mwork.ParameterContainer;
 import eu.mshade.mwork.event.EventListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 
+public class PacketRequestChunkHandler implements EventListener<PacketMoveEvent> {
 
-public class PacketMoveHandler implements EventListener<PacketMoveEvent> {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(PacketMoveHandler.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(PacketRequestChunkHandler.class);
     private DedicatedEnderChest dedicatedEnderChest;
 
-    public PacketMoveHandler(DedicatedEnderChest dedicatedEnderChest) {
+    public PacketRequestChunkHandler(DedicatedEnderChest dedicatedEnderChest) {
         this.dedicatedEnderChest = dedicatedEnderChest;
     }
 
@@ -30,18 +26,13 @@ public class PacketMoveHandler implements EventListener<PacketMoveEvent> {
         EnderFrameSessionHandler enderFrameSessionHandler = parameterContainer.getContainer(EnderFrameSessionHandler.class);
         EnderFrameSession enderFrameSession = enderFrameSessionHandler.getEnderFrameSession();
         Player player = enderFrameSession.getPlayer();
-        Location before = player.getLocation();
+        Location before = player.getBeforeLocation();
         Location now = player.getLocation();
 
         if (before.getChunkX() != now.getChunkX() || before.getChunkZ() != now.getChunkZ()) {
-            int render = before.distance(now) < 5 ? 10 : 3;
+            int render = (before.distance(now) < 5 ? 10 : 3);
             enderFrameSession.sendSquareChunk(render, now.getChunkX(), now.getChunkZ(), now.getWorld());
         }
-
-        for (Player viewer : player.getViewers()) {
-            viewer.getEnderFrameSessionHandler().getEnderFrameSession().moveTo(player);
-        }
-
 
     }
 
