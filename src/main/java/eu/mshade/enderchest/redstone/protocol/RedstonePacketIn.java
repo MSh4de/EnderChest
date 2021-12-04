9730821904fb7e217841803caa3d78b6ca6f9dd1
RealAlphaUA <realalpha.ua@gmail.com>
2021-12-04 23:52:54 +0100

    delete redstone 🔥

--- src/main/java/eu/mshade/enderchest/redstone/protocol/RedstonePacketIn.java
+++
@@ -1,9 +0,0 @@
-package eu.mshade.enderchest.redstone.protocol;
-
-public abstract class RedstonePacketIn extends RedstonePacket {
-
-
-    public RedstonePacketIn() {
-        super(RedstonePacketType.UNKNOW, 0);
-    }
-}
--- src/main/java/eu/mshade/enderchest/redstone/protocol/packet/RedstonePacketInUnknow.java
+++
@@ -1,6 +0,0 @@
-package eu.mshade.enderchest.redstone.protocol.packet;
-
-import eu.mshade.enderchest.redstone.protocol.RedstonePacketIn;
-
-public class RedstonePacketInUnknow extends RedstonePacketIn {
-}
