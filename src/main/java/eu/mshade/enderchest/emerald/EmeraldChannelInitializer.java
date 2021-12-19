1630b4d4e787f9b2dc5f8fa1c7f0e1349ef91bd2
RealAlphaUA <realalpha.ua@gmail.com>
2021-12-19 23:13:19 +0100

    add EmeraldChannelInitializer 🎠

---
+++ src/main/java/eu/mshade/enderchest/emerald/EmeraldChannelInitializer.java
@@ -0,0 +1,30 @@
+package eu.mshade.enderchest.emerald;
+
+import eu.mshade.mwork.MWork;
+import eu.mshade.shulker.Shulker;
+import eu.mshade.shulker.protocol.ShulkerPacketCodec;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+
+import java.util.concurrent.TimeUnit;
+
+public class EmeraldChannelInitializer extends ChannelInitializer<Channel> {
+
+    private Shulker shulker;
+    private MWork mWork;
+
+    public EmeraldChannelInitializer(Shulker shulker, MWork mWork) {
+        this.shulker = shulker;
+        this.mWork = mWork;
+    }
+
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        ch.pipeline()
+                .addLast("timeout", new ReadTimeoutHandler(0, TimeUnit.MILLISECONDS))
+                .addLast("codecs", new ShulkerPacketCodec(shulker.getShulkerPacketRepository(), mWork))
+                .addLast("handler", new EmeraldSessionHandler(ch, shulker));
+
+    }
+}
