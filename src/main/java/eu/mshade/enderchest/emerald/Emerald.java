ac15932028b856ea6213d87a5556e14f20f2e4ae
RealAlphaUA <realalpha.ua@gmail.com>
2021-12-31 18:30:55 +0100

    add Emerald 🦺

---
+++ src/main/java/eu/mshade/enderchest/emerald/Emerald.java
@@ -0,0 +1,121 @@
+package eu.mshade.enderchest.emerald;
+
+import eu.mshade.enderchest.emerald.listener.ShulkerPacketJoinServiceListener;
+import eu.mshade.enderchest.emerald.listener.ShulkerPacketLeaveServiceListener;
+import eu.mshade.enderchest.emerald.listener.ShulkerPacketLoginResponseListener;
+import eu.mshade.enderchest.emerald.listener.ShulkerPacketSubscribeListener;
+import eu.mshade.mwork.MWork;
+import eu.mshade.mwork.event.EventBus;
+import eu.mshade.shulker.Shulker;
+import eu.mshade.shulker.ShulkerService;
+import eu.mshade.shulker.event.ShulkerEventBus;
+import eu.mshade.shulker.packet.*;
+import eu.mshade.shulker.protocol.*;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Queue;
+import java.util.concurrent.*;
+
+public class Emerald {
+
+    private static Logger logger = LoggerFactory.getLogger(Emerald.class);
+    private EventLoopGroup nioEventLoopGroup;
+    private ShulkerServiceRepository shulkerServiceRepository = new ShulkerServiceRepository();
+    private TopicRepository topicRepository = new TopicRepository();
+    private ShulkerService shulkerService;
+    private Channel channel;
+    private Shulker shulker;
+    private BlockingQueue<ShulkerPacketContainer<ShulkerPacket>> shulkerPacketContainerQueue = new LinkedBlockingDeque<>();
+
+    public Emerald(EventLoopGroup nioEventLoopGroup) {
+        this.nioEventLoopGroup = nioEventLoopGroup;
+
+        MWork mWork = MWork.get();
+        this.shulker = new Shulker(mWork);
+
+
+        nioEventLoopGroup.execute(() -> {
+            while (true){
+                try {
+                    ShulkerPacketContainer<ShulkerPacket> shulkerPacketContainer = shulkerPacketContainerQueue.take();
+                    sendPacket(shulkerPacketContainer);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        ShulkerEventBus shulkerPacketEventBus = shulker.getShulkerPacketEventBus();
+
+        shulkerPacketEventBus.subscribe(ShulkerPacketLoginResponse.class, new ShulkerPacketLoginResponseListener(this));
+        shulkerPacketEventBus.subscribe(ShulkerPacketJoinService.class, new ShulkerPacketJoinServiceListener(this));
+        shulkerPacketEventBus.subscribe(ShulkerPacketLeaveService.class, new ShulkerPacketLeaveServiceListener(this));
+        shulkerPacketEventBus.subscribe(ShulkerPacketSubscribe.class, new ShulkerPacketSubscribeListener(this));
+
+        Bootstrap bootstrap = new Bootstrap()
+                .group(nioEventLoopGroup)
+                .channel(NioSocketChannel.class)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .handler(new EmeraldChannelInitializer(shulker, mWork));
+
+        try {
+            channel = bootstrap.connect("localhost", 4568).addListener(future -> {
+                sendPacket(new ShulkerPacketLoginRequest("BFkDDhdjhrc2URsIosOiI9frFMDXJHBq3DJC3XlWhdpyjAxUSyaFrPC7X6TPixEbcGMveCMtsuUL2nD6gDGx/YRqrTFHyvytqLdgJlfY6QA="));
+                logger.info(String.valueOf(channel));
+            }).channel();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void sendPacket(Topic topic, ShulkerPacket shulkerPacket){
+        if (!channel.isActive() && !channel.isOpen()) return;
+                ShulkerPacketHeader shulkerPacketHeader = new ShulkerPacketHeader(this.shulker.getShulkerPacketRepository(), topic.getId(), this.shulkerService.getId(), shulkerService.getId(), topic.getShulkerPacketType());
+                shulkerPacketContainerQueue.add(new ShulkerPacketContainer<>(shulkerPacketHeader, shulkerPacket));
+                //this.channel.writeAndFlush(new ShulkerPacketContainer<>(shulkerPacketHeader, shulkerPacket));
+
+
+        /*
+        this.getShulkerServiceRepository().getSubscriber(topic.getShulkerPacketType()).forEach(shulkerService -> {
+            ShulkerPacketHeader shulkerPacketHeader = new ShulkerPacketHeader(this.shulker.getShulkerPacketRepository(), topic.getId(), this.shulkerService.getId(), shulkerService.getId(), topic.getShulkerPacketType());
+            this.channel.writeAndFlush(new ShulkerPacketContainer<>(shulkerPacketHeader, shulkerPacket));
+        });
+
+         */
+    }
+
+
+    public void sendPacket(ShulkerPacketContainer<ShulkerPacket> shulkerPacketShulkerPacketContainer){
+        this.channel.writeAndFlush(shulkerPacketShulkerPacketContainer, channel.voidPromise());
+    }
+
+    public void sendPacket(ShulkerPacket shulkerPacket){
+        if (!channel.isActive() && !channel.isOpen()) return;
+        ShulkerPacketType packetTypeByPacket = this.shulker.getShulkerPacketRepository().getPacketTypeByPacket(shulkerPacket);
+        ShulkerPacketHeader shulkerPacketHeader = new ShulkerPacketHeader(this.shulker.getShulkerPacketRepository(), 0, 0, packetTypeByPacket);
+        this.channel.writeAndFlush(new ShulkerPacketContainer<>(shulkerPacketHeader, shulkerPacket), channel.voidPromise());
+    }
+
+    public ShulkerService getShulkerService() {
+        return shulkerService;
+    }
+
+    public void setShulkerService(ShulkerService shulkerService) {
+        this.shulkerService = shulkerService;
+    }
+
+    public ShulkerServiceRepository getShulkerServiceRepository() {
+        return shulkerServiceRepository;
+    }
+
+    public TopicRepository getTopicRepository() {
+        return topicRepository;
+    }
+}
