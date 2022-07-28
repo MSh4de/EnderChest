dd8408767f592c268df78b5e1b05e31fb6b0a335
RealAlphaUA <realalpha.ua@gmail.com>
2022-07-29 00:17:01 +0200

    add ChunkMediator 🎨

---
+++ src/main/java/eu/mshade/enderchest/world/ChunkMediator.java
@@ -0,0 +1,19 @@
+package eu.mshade.enderchest.world;
+
+import eu.mshade.enderframe.world.Chunk;
+import eu.mshade.mwork.mediator.Mediator;
+
+import java.util.function.Supplier;
+
+public class ChunkMediator implements Mediator<Chunk> {
+
+
+    public static ChunkMediator CHUNK_MEDIATOR = new ChunkMediator();
+
+    @Override
+    public <T> T notify(Chunk source, Supplier<T> callback, String key, Object[] args) {
+        //System.out.println(key);
+        return callback.get();
+    }
+
+}
