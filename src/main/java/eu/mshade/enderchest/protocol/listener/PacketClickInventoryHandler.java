5d64ec1bcc680fefcf4a07bdbe31c342ecb37fc8
RealAlphaUA <realalpha.ua@gmail.com>
2022-08-02 03:09:23 +0200

    add tracker of drag clicktype 🎨

--- src/main/java/eu/mshade/enderchest/protocol/listener/PacketClickInventoryHandler.java
+++ src/main/java/eu/mshade/enderchest/protocol/listener/PacketClickInventoryHandler.java
@@ -2,16 +2,18 @@ package eu.mshade.enderchest.protocol.listener;
 
 import eu.mshade.enderframe.GameMode;
 import eu.mshade.enderframe.entity.Player;
-import eu.mshade.enderframe.inventory.ClickType;
-import eu.mshade.enderframe.inventory.Inventory;
-import eu.mshade.enderframe.inventory.InventoryBufferStore;
+import eu.mshade.enderframe.inventory.*;
 import eu.mshade.enderframe.item.ItemStack;
+import eu.mshade.enderframe.item.Material;
 import eu.mshade.enderframe.packetevent.PacketClickInventoryEvent;
 import eu.mshade.enderframe.protocol.ProtocolPipeline;
 import eu.mshade.enderframe.protocol.SessionWrapper;
 import eu.mshade.mwork.ParameterContainer;
 import eu.mshade.mwork.event.EventListener;
 import io.netty.channel.Channel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PacketClickInventoryHandler implements EventListener<PacketClickInventoryEvent> {
 
@@ -24,53 +26,221 @@ public class PacketClickInventoryHandler implements EventListener<PacketClickInv
         Inventory inventory = event.getInventory();
         InventoryBufferStore inventoryBufferStore = player.getInventoryBufferStore();
         ClickType clickType = event.getClickType();
+        int slot = event.getSlot();
+
 
         switch (clickType) {
             case LEFT -> {
-                if(inventoryBufferStore.getPickedItemStack() == null) {
-                    ItemStack pickedItemStack = inventory.getItemStack(event.getSlot());
+                if (inventoryBufferStore.getPickedItemStack() == null) {
+                    ItemStack pickedItemStack = inventory.getItemStack(slot);
                     inventoryBufferStore.setPickedItemStack(pickedItemStack);
-                    inventory.deleteItemStack(event.getSlot());
-                }else {
+                    inventory.deleteItemStack(slot);
+                } else {
+                    ItemStack itemStack = inventory.getItemStack(slot);
                     ItemStack pickedItemStack = inventoryBufferStore.getPickedItemStack();
-                    inventory.setItemStack(event.getSlot(), pickedItemStack);
-                    inventoryBufferStore.setPickedItemStack(null);
-                    System.out.println("SET ITEM "+pickedItemStack+" in slot "+event.getSlot());
+                    if (itemStack != null) {
+                        if (itemStack.getMaterial().equals(pickedItemStack.getMaterial())) {
+                            if (itemStack.getAmount() != 64 && pickedItemStack.getAmount() != 64) {
+                                int totalAmount = itemStack.getAmount() + pickedItemStack.getAmount();
+                                int remainAmount = totalAmount % 64;
+                                if (totalAmount >= 64) {
+                                    itemStack.setAmount(64);
+                                    inventoryBufferStore.setPickedItemStack(pickedItemStack.clone().setAmount(remainAmount));
+                                } else {
+                                    itemStack.setAmount(totalAmount);
+                                    inventoryBufferStore.setPickedItemStack(null);
+                                }
+                            }
+                        } else {
+                            inventoryBufferStore.setPickedItemStack(itemStack);
+                            inventory.setItemStack(slot, pickedItemStack);
+                        }
+                    } else {
+                        inventoryBufferStore.setPickedItemStack(null);
+                        inventory.setItemStack(slot, pickedItemStack);
+                    }
                 }
             }
             case RIGHT -> {
                 if (inventoryBufferStore.getPickedItemStack() == null) {
-                    ItemStack itemStack = inventory.getItemStack(event.getSlot());
-                    if (itemStack != null){
+                    ItemStack itemStack = inventory.getItemStack(slot);
+                    if (itemStack != null) {
                         int amountOfPickedIemStack = itemStack.getAmount() / 2;
                         int amountOfInventoryItem = amountOfPickedIemStack - (itemStack.getAmount() % 2);
                         itemStack.modifyAmount(integer -> amountOfInventoryItem);
                         inventoryBufferStore.setPickedItemStack(itemStack.clone().setAmount(amountOfPickedIemStack));
                     }
-                }else {
+                } else {
                     ItemStack pickedItemStack = inventoryBufferStore.getPickedItemStack();
-                    pickedItemStack.modifyAmount(integer -> integer-1);
-                    if (pickedItemStack.getAmount() <= 0){
+                    pickedItemStack.modifyAmount(integer -> integer - 1);
+                    if (pickedItemStack.getAmount() <= 0) {
                         inventoryBufferStore.setPickedItemStack(null);
                     }
-                    ItemStack itemStack = inventory.getItemStack(event.getSlot());
+                    ItemStack itemStack = inventory.getItemStack(slot);
                     if (itemStack == null) {
-                        inventory.setItemStack(event.getSlot(), pickedItemStack.clone()
+                        inventory.setItemStack(slot, pickedItemStack.clone()
                                 .setAmount(1));
                     } else {
-                        itemStack.modifyAmount(integer -> integer+1);
+                        itemStack.modifyAmount(integer -> integer + 1);
+                    }
+                }
+            }
+            case DOUBLE_CLICK -> {
+                if (inventoryBufferStore.getPickedItemStack() == null) return;
+                ItemStack pickedItemStack = inventoryBufferStore.getPickedItemStack();
+                if (player.getOpenedInventory() != null) {
+                    Inventory openedInventory = player.getOpenedInventory();
+                    onMiddleClick(pickedItemStack, openedInventory);
+                }
+
+                if (pickedItemStack.getAmount() != 64) {
+                    onMiddleClick(pickedItemStack, player.getPlayerInventory());
+                }
+
+
+            }
+            case ADD_ITEM_FROM_DRAG ->
+                    inventoryBufferStore.addPlacedItemStack(slot, inventory instanceof PlayerInventory);
+            case START_DRAG_LEFT -> inventoryBufferStore.setLastClickType(ClickType.START_DRAG_LEFT);
+            case END_DRAG_LEFT -> {
+                if (inventoryBufferStore.getLastClickType() != ClickType.START_DRAG_LEFT) return;
+                ItemStack pickedItemStack = inventoryBufferStore.getPickedItemStack();
+
+                int size = inventoryBufferStore.getPLacedItemStacks().size();
+                int amountOfPickedIemStack = pickedItemStack.getAmount() % size;
+                int amountOfInventoryItem = (pickedItemStack.getAmount() / size);
+                pickedItemStack.setAmount(amountOfPickedIemStack);
+                if (amountOfPickedIemStack == 0) inventoryBufferStore.setPickedItemStack(null);
+
+
+                for (InventoryBufferStore.PlacedItemStack placedItemStack : inventoryBufferStore.getPLacedItemStacks()) {
+                    Inventory inventoryTarget = (placedItemStack.isPlayerInventory() ? player.getPlayerInventory() : player.getOpenedInventory());
+
+                    ItemStack itemStack = inventoryTarget.getItemStack(placedItemStack.getSlot());
+                    if (itemStack == null) {
+                        inventoryTarget.setItemStack(placedItemStack.getSlot(), pickedItemStack
+                                .clone()
+                                .setAmount(amountOfInventoryItem));
+                    } else {
+                        itemStack.modifyAmount(integer -> integer + amountOfInventoryItem);
+                    }
+
+                }
+
+                inventoryBufferStore.clearPLacedItemStacks();
+            }
+            case START_DRAG_RIGHT -> inventoryBufferStore.setLastClickType(ClickType.START_DRAG_RIGHT);
+            case END_DRAG_RIGHT -> {
+                if (inventoryBufferStore.getLastClickType() != ClickType.START_DRAG_RIGHT) return;
+                ItemStack pickedItemStack = inventoryBufferStore.getPickedItemStack();
+                int size = inventoryBufferStore.getPLacedItemStacks().size();
+                pickedItemStack.modifyAmount(integer -> integer - size);
+                if (pickedItemStack.getAmount() <= 0) inventoryBufferStore.setPickedItemStack(null);
+
+                for (InventoryBufferStore.PlacedItemStack placedItemStack : inventoryBufferStore.getPLacedItemStacks()) {
+                    Inventory inventoryTarget = (placedItemStack.isPlayerInventory() ? player.getPlayerInventory() : player.getOpenedInventory());
+
+                    ItemStack itemStack = inventoryTarget.getItemStack(placedItemStack.getSlot());
+                    if (itemStack == null) {
+                        inventoryTarget.setItemStack(placedItemStack.getSlot(), pickedItemStack.clone()
+                                .setAmount(1));
+                    } else {
+                        itemStack.modifyAmount(integer -> integer + 1);
+                    }
+
+                }
+
+                inventoryBufferStore.clearPLacedItemStacks();
+            }
+            case START_DRAG_MIDDLE -> inventoryBufferStore.setLastClickType(ClickType.START_DRAG_MIDDLE);
+            case END_DRAG_MIDDLE -> {
+                if (inventoryBufferStore.getLastClickType() != ClickType.START_DRAG_MIDDLE) return;
+                ItemStack pickedItemStack = inventoryBufferStore.getPickedItemStack();
+                inventoryBufferStore.setPickedItemStack(null);
+
+                for (InventoryBufferStore.PlacedItemStack placedItemStack : inventoryBufferStore.getPLacedItemStacks()) {
+                    Inventory inventoryTarget = (placedItemStack.isPlayerInventory() ? player.getPlayerInventory() : player.getOpenedInventory());
+                    ItemStack itemStack = inventoryTarget.getItemStack(placedItemStack.getSlot());
+                    if (itemStack == null){
+                        inventoryTarget.setItemStack(placedItemStack.getSlot(), pickedItemStack.clone());
+                    }else {
+                        itemStack.setAmount(pickedItemStack.getAmount());
+                    }
+                }
+                inventoryBufferStore.clearPLacedItemStacks();
+            }
+            case MIDDLE -> {
+                if (player.getGameMode() == GameMode.CREATIVE) {
+                    ItemStack itemStack = inventory.getItemStack(slot);
+                    if (itemStack != null && !itemStack.getMaterial().equals(Material.AIR)) {
+                        inventoryBufferStore.setPickedItemStack(itemStack
+                                .clone()
+                                .setAmount(64));
+                    }
+                }
+            }
+            case NUMBER_KEY -> {
+                int key = event.getKey();
+                PlayerInventory playerInventory = player.getPlayerInventory();
+                ItemStack itemStackFromHotBar = playerInventory.getItemStack(key);
+                ItemStack itemStackFromInventory = inventory.getItemStack(slot);
+
+                if (inventory instanceof PlayerInventory) {
+                    inventory.setItemStack(key, itemStackFromInventory);
+                    inventory.setItemStack(slot, itemStackFromHotBar);
+                } else {
+                    if (itemStackFromInventory == null) {
+                        inventory.setItemStack(slot, itemStackFromHotBar);
+                        playerInventory.deleteItemStack(key);
+                    } else if (itemStackFromHotBar == null) {
+                        playerInventory.setItemStack(key, itemStackFromInventory);
+                        inventory.deleteItemStack(slot);
+                        break;
+                    }
+
+                    for (int i = 0; i < playerInventory.getItemStacks().length; i++) {
+                        ItemStack itemStack = playerInventory.getItemStack(i);
+
                     }
                 }
             }
         }
-        //System.out.println(event);
-        //System.out.println(Arrays.toString(inventory.getItemStacks()));
-
-        if (player.getGameMode() == GameMode.CREATIVE && clickType == ClickType.MIDDLE) {
-        }
 
 
-        //System.out.println(event);
     }
 
+    private void onMiddleClick(ItemStack pickedItemStack, Inventory inventory) {
+
+        List<InventoryBufferStore.PlacedItemStack> placedItemStacks = new ArrayList<>();
+
+        for (int i = 0; i < inventory.getItemStacks().length; i++) {
+            ItemStack itemStackTarget = inventory.getItemStack(i);
+            if (itemStackTarget != null && itemStackTarget.getMaterial().equals(pickedItemStack.getMaterial()) && itemStackTarget.getAmount() != 64) {
+                placedItemStacks.add(new InventoryBufferStore.PlacedItemStack(i));
+            }
+        }
+
+        for (int i = 0; i < inventory.getItemStacks().length; i++) {
+            ItemStack itemStackTarget = inventory.getItemStack(i);
+            if (itemStackTarget != null && itemStackTarget.getMaterial().equals(pickedItemStack.getMaterial()) && itemStackTarget.getAmount() == 64) {
+                placedItemStacks.add(new InventoryBufferStore.PlacedItemStack(i));
+            }
+        }
+
+        for (InventoryBufferStore.PlacedItemStack placedItemStack : placedItemStacks) {
+            int slot = placedItemStack.getSlot();
+            ItemStack itemStack = inventory.getItemStack(slot);
+            int remainAmount = 64 - pickedItemStack.getAmount();
+            if (itemStack.getAmount() <= remainAmount) {
+                pickedItemStack.modifyAmount(integer -> integer + itemStack.getAmount());
+                inventory.deleteItemStack(slot);
+            } else {
+                itemStack.modifyAmount(integer -> integer - remainAmount);
+                pickedItemStack.modifyAmount(integer -> integer + remainAmount);
+            }
+
+            if (pickedItemStack.getAmount() == 64) break;
+        }
+
+    }
 }
