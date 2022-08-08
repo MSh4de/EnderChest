89b83056902be960e4c2c0bbe555a46ed6f6dcab
RealAlphaUA <realalpha.ua@gmail.com>
2022-08-08 22:59:14 +0200

    change interface to abstract class 🦺

--- src/main/java/eu/mshade/enderchest/world/DefaultSection.java
+++ src/main/java/eu/mshade/enderchest/world/DefaultSection.java
@@ -8,18 +8,10 @@ import eu.mshade.enderframe.world.Section;
 import java.util.Arrays;
 import java.util.Objects;
 
-public class DefaultSection implements Section {
-
-    private Chunk chunk;
-    private int y;
-    private int realBlock;
-    private int[] blocks;
-    private NibbleArray blockLight;
-    private NibbleArray skyLight;
+public class DefaultSection extends Section {
 
     public DefaultSection(Chunk chunk, int y, int realBlock) {
         this(chunk, y, realBlock, new int[4096], NibbleArray.allocate(4096), NibbleArray.allocate(4096));
-        // check if world is overlord(get from metadataKeyValue) and complete skylight
         if (chunk.getWorld().getDimension() == Dimension.OVERWORLD) {
             skyLight.fill((byte) 15);
         }
@@ -28,52 +20,10 @@ public class DefaultSection implements Section {
     }
 
     public DefaultSection(Chunk chunk, int y, int realBlock, int[] blocks, NibbleArray blocksLight, NibbleArray skyLight) {
-        this.chunk = chunk;
-        this.y = y;
-        this.realBlock = realBlock;
-        this.blocks = blocks;
-        this.blockLight = blocksLight;
-        this.skyLight = skyLight;
-    }
-
-    @Override
-    public int getY() {
-        return y;
-    }
-
-    @Override
-    public Chunk getChunk() {
-        return chunk;
-    }
-
-    @Override
-    public int getRealBlock() {
-        return realBlock;
-    }
-
-    @Override
-    public void setRealBlock(int realBlock) {
+        super(chunk, y, blocks, blocksLight, skyLight);
         this.realBlock = realBlock;
     }
 
-    @Override
-    public int[] getBlocks() {
-        return blocks;
-    }
-
-    @Override
-    public NibbleArray getBlockLight() {
-        return blockLight;
-    }
-
-    @Override
-    public NibbleArray getSkyLight() {
-        return skyLight;
-    }
-
-    public void setBlocks(int[] blocks) {
-        this.blocks = blocks;
-    }
 
     @Override
     public String toString() {
