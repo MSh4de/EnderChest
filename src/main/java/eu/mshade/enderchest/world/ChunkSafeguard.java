d6c12f66824ec5c2cf36960bc03fb78d12297f42
RealAlphaUA <realalpha.ua@gmail.com>
2022-08-11 01:15:08 +0200

    change Runnable by Thread 🧶

--- src/main/java/eu/mshade/enderchest/world/ChunkSafeguard.java
+++ src/main/java/eu/mshade/enderchest/world/ChunkSafeguard.java
@@ -1,32 +1,45 @@
 package eu.mshade.enderchest.world;
 
+import eu.mshade.enderframe.world.ChunkStatus;
 import eu.mshade.enderframe.world.chunk.Chunk;
+import eu.mshade.enderframe.world.chunk.ChunkStateStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
+import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
-public class ChunkSafeguard implements Runnable {
+public class ChunkSafeguard extends Thread {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ChunkSafeguard.class);
-    protected BlockingQueue<Chunk> chunkBlockingQueue = new LinkedBlockingQueue<>();
+    protected Queue<Chunk> chunkBlockingQueue = new ConcurrentLinkedQueue<>();
+
+    public ChunkSafeguard() {
+        super("ChunkSafeguard");
+    }
 
     @Override
     public void run() {
         while (true) {
-            try {
-                Chunk chunk = chunkBlockingQueue.take();
-                chunk.getWorld().saveChunk(chunk);
-                chunk.getInChunkSafeguard().set(false);
-            } catch (InterruptedException e) {
-                LOGGER.error("ChunkSafeguard interrupted", e);
+            Chunk chunk = chunkBlockingQueue.poll();
+            if (chunk == null) {
+                continue;
             }
+            chunk.getWorld().saveChunk(chunk);
+            ChunkStateStore chunkStateStore = chunk.getChunkStateStore();
+            chunkStateStore.leaveChunkSafeguard();
+            if (chunkStateStore.getChunkStatus() == ChunkStatus.PREPARE_TO_UNLOAD) chunkStateStore.finishWrite();
         }
+
     }
     public void addChunk(Chunk chunk) {
-        chunk.getInChunkSafeguard().set(true);
+        chunk.getChunkStateStore().joinChunkSafeguard();
         chunkBlockingQueue.add(chunk);
     }
+
 
 }
