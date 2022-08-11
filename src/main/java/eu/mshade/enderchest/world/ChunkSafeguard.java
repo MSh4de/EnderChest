9012303bcc7e281fe01736431cd71bdbd6091de5
RealAlphaUA <realalpha.ua@gmail.com>
2022-08-11 02:21:48 +0200

    clean up stopSafeguard 🎠

--- src/main/java/eu/mshade/enderchest/world/ChunkSafeguard.java
+++ src/main/java/eu/mshade/enderchest/world/ChunkSafeguard.java
@@ -8,15 +8,14 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 import java.util.Queue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.*;
 
 public class ChunkSafeguard extends Thread {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ChunkSafeguard.class);
-    protected Queue<Chunk> chunkBlockingQueue = new ConcurrentLinkedQueue<>();
+    protected BlockingQueue<Chunk> chunkBlockingQueue = new LinkedBlockingQueue<>();
+    protected boolean running = true;
+    protected CompletableFuture<Void> waitingFinishLastChunk = new CompletableFuture<>();
 
     public ChunkSafeguard() {
         super("ChunkSafeguard");
@@ -24,18 +23,33 @@ public class ChunkSafeguard extends Thread {
 
     @Override
     public void run() {
-        while (true) {
-            Chunk chunk = chunkBlockingQueue.poll();
-            if (chunk == null) {
-                continue;
+        while (running) {
+            try {
+                Chunk chunk = chunkBlockingQueue.take();
+                chunk.getWorld().saveChunk(chunk);
+                ChunkStateStore chunkStateStore = chunk.getChunkStateStore();
+                chunkStateStore.leaveChunkSafeguard();
+                if (chunkStateStore.getChunkStatus() == ChunkStatus.PREPARE_TO_UNLOAD) chunkStateStore.finishWrite();
+                if (!running) {
+                    waitingFinishLastChunk.complete(null);
+                }
+            }catch (Exception e) {
+                LOGGER.error("Impossible to take chunk", e);
             }
-            chunk.getWorld().saveChunk(chunk);
-            ChunkStateStore chunkStateStore = chunk.getChunkStateStore();
-            chunkStateStore.leaveChunkSafeguard();
-            if (chunkStateStore.getChunkStatus() == ChunkStatus.PREPARE_TO_UNLOAD) chunkStateStore.finishWrite();
         }
 
     }
+
+    public void stopSafeguard() {
+        running = false;
+        LOGGER.info("ChunkSafeguard is finished");
+        try {
+            waitingFinishLastChunk.get();
+        } catch (InterruptedException | ExecutionException e) {
+            LOGGER.error("Impossible to waiting last chunk", e);
+        }
+    }
+
     public void addChunk(Chunk chunk) {
         chunk.getChunkStateStore().joinChunkSafeguard();
         chunkBlockingQueue.add(chunk);
