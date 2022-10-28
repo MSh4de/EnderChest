a298963abc6c9a33912e1b0a118e974f037e08d0
RealAlphaUA <realalpha.ua@gmail.com>
2022-10-28 23:05:49 +0200

    test schematic mc 🎠

--- src/main/test/java/Test.java
+++ src/main/test/java/Test.java
@@ -1,23 +1,43 @@
+import eu.mshade.enderframe.item.MaterialKey;
+import eu.mshade.enderman.wrapper.EndermanMaterialKeyWrapper;
+import eu.mshade.mwork.binarytag.BinaryTagDriver;
+import eu.mshade.mwork.binarytag.entity.CompoundBinaryTag;
+
+import java.io.*;
+import java.util.zip.GZIPInputStream;
 
 public class Test {
 
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
+        BinaryTagDriver binaryTagDriver = new BinaryTagDriver();
+        GZIPInputStream gzipInputStream = new GZIPInputStream(new FileInputStream("C:/Users/reala/Downloads/spawn-e1930.schematic"));
+        CompoundBinaryTag compoundBinaryTag = binaryTagDriver.readCompoundBinaryTag(gzipInputStream);
+        System.out.println(compoundBinaryTag.getValue().keySet());
+        System.out.println(compoundBinaryTag.getBinaryTag("Platform"));
+        System.out.println(compoundBinaryTag.getBinaryTag("Materials"));
+        byte[] blocks = compoundBinaryTag.getByteArray("Blocks");
+        byte[] data = compoundBinaryTag.getByteArray("Data");
 
-        System.out.println(17 % 2);
+        EndermanMaterialKeyWrapper endermanMaterialKeyWrapper = new EndermanMaterialKeyWrapper();
 
-        /*
-        scheduledExecutorService.scheduleAtFixedRate(() -> {
-            logger.info("hey");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+        int unknown = 0;
+        for (int i = 0; i < blocks.length; i++) {
+            byte block = blocks[i];
+            MaterialKey materialKey = MaterialKey.from(block);
+            MaterialKey reverse = endermanMaterialKeyWrapper.reverse(materialKey);
+            if (reverse == null){
+                unknown++;
+                System.out.println("Unknown block: " + materialKey);
             }
-        }, 0, 1, TimeUnit.NANOSECONDS);
+        }
 
-         */
+        double percentage = (double)unknown / (double)blocks.length;
 
+        System.out.println(percentage * 100 + "% unknown");
+
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println((1 << 31));
     }
 
     public static int getIndex(int x, int y, int z){
