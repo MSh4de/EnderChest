98044bcda6da3c3e809777afd8c5fbe61b8cd95f
nullptr-rs <louisdechorivit@gmail.com>
2022-12-20 21:37:55 +0100

    :hammer: Scheduled push

--- src/main/java/eu/mshade/enderchest/protocol/listener/PacketClickInventoryHandler.java
+++ src/main/java/eu/mshade/enderchest/protocol/listener/PacketClickInventoryHandler.java
@@ -3,14 +3,11 @@ package eu.mshade.enderchest.protocol.listener;
 import eu.mshade.enderframe.GameMode;
 import eu.mshade.enderframe.entity.Player;
 import eu.mshade.enderframe.inventory.*;
+import eu.mshade.enderframe.inventory.type.PlayerInventory;
 import eu.mshade.enderframe.item.ItemStack;
 import eu.mshade.enderframe.item.Material;
 import eu.mshade.enderframe.packetevent.PacketClickInventoryEvent;
-import eu.mshade.enderframe.protocol.ProtocolPipeline;
-import eu.mshade.enderframe.protocol.SessionWrapper;
-import eu.mshade.mwork.ParameterContainer;
 import eu.mshade.mwork.event.EventListener;
-import io.netty.channel.Channel;
 
 import java.util.ArrayList;
 import java.util.List;
