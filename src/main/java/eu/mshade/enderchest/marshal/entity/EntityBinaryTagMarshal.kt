7333d5bb00007aeb4fc001bd555369cce6c1d5c1
RealAlphaUA <realalpha.ua@gmail.com>
2023-01-19 17:20:31 +0100

    fix type 🛒

--- src/main/java/eu/mshade/enderchest/marshal/entity/EntityBinaryTagMarshal.kt
+++ src/main/java/eu/mshade/enderchest/marshal/entity/EntityBinaryTagMarshal.kt
@@ -1,12 +1,12 @@
 package eu.mshade.enderchest.marshal.entity
 
 import eu.mshade.enderchest.entity.DefaultSheep
-import eu.mshade.enderchest.marshal.metadata.MetadataKeyValueBinaryTagMarshal
 import eu.mshade.enderchest.marshal.world.LocationBinaryTagMarshal
 import eu.mshade.enderchest.marshal.world.VectorBinaryTagMarshal
 import eu.mshade.enderframe.entity.Entity
 import eu.mshade.enderframe.entity.EntityKey
 import eu.mshade.enderframe.entity.EntityType
+import eu.mshade.enderframe.metadata.MetadataKeyValueBufferRegistry
 import eu.mshade.enderframe.world.Location
 import eu.mshade.enderframe.world.Vector
 import eu.mshade.mwork.binarytag.entity.CompoundBinaryTag
@@ -20,17 +20,17 @@ object EntityBinaryTagMarshal {
         entityTypes[EntityType.SHEEP] = {location, velocity, entityId, uuid -> DefaultSheep(location, velocity, entityId, uuid)}
     }
 
-    fun serialize(entity: Entity, metadataKeyValueBinaryTagMarshal: MetadataKeyValueBinaryTagMarshal): CompoundBinaryTag {
+    fun serialize(entity: Entity, metadataKeyValueBinaryTagMarshal: MetadataKeyValueBufferRegistry): CompoundBinaryTag {
         val compound = CompoundBinaryTag()
 
         compound.putString("type", entity.entityKey.getName())
-        compound.putBinaryTag("location", LocationBinaryTagMarshal().serialize(entity.location))
+        compound.putBinaryTag("location", LocationBinaryTagMarshal.serialize(entity.location))
         compound.putBinaryTag("metadataKeyValueBucket", metadataKeyValueBinaryTagMarshal.serialize(entity.metadataKeyValueBucket))
         compound.putBinaryTag("velocity", VectorBinaryTagMarshal.serialize(entity.velocity))
         return compound
     }
 
-    fun deserialize(compoundBinaryTag: CompoundBinaryTag, metadataKeyValueBinaryTagMarshal: MetadataKeyValueBinaryTagMarshal): Entity? {
+    fun deserialize(compoundBinaryTag: CompoundBinaryTag, metadataKeyValueBinaryTagMarshal: MetadataKeyValueBufferRegistry): Entity? {
         return null
     }
 }
