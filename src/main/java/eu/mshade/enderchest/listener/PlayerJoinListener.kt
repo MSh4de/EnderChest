4b93ce1efae7fb0337e58b185750ab4e420c7f61
RealAlphaUA <realalpha.ua@gmail.com>
2023-07-14 01:01:39 +0200

    update 🧨

--- src/main/java/eu/mshade/enderchest/listener/PlayerJoinListener.kt
+++ src/main/java/eu/mshade/enderchest/listener/PlayerJoinListener.kt
@@ -1,25 +1,12 @@
 package eu.mshade.enderchest.listener
 
 import eu.mshade.enderchest.EnderChest
-import eu.mshade.enderframe.attribute.Attribute
-import eu.mshade.enderframe.attribute.AttributeModifier
-import eu.mshade.enderframe.attribute.AttributeOperation
-import eu.mshade.enderframe.effect.Effect
-import eu.mshade.enderframe.effect.EffectType
 import eu.mshade.enderframe.event.PlayerJoinEvent
-import eu.mshade.enderframe.inventory.EquipmentSlot
 import eu.mshade.enderframe.item.ItemStack
-import eu.mshade.enderframe.item.ItemStackAttributeModifier
 import eu.mshade.enderframe.item.Material
-import eu.mshade.enderframe.item.metadata.AttributeModifiersItemStackMetadata
-import eu.mshade.enderframe.item.metadata.MapDecorationsItemStackMetadata
-import eu.mshade.enderframe.item.metadata.MapIsScalingItemStackMetadata
 import eu.mshade.enderframe.map.MapCursorType
 import eu.mshade.enderframe.map.MapDecoration
 import eu.mshade.mwork.event.EventListener
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
 import org.slf4j.LoggerFactory
 
 class PlayerJoinListener : EventListener<PlayerJoinEvent> {
